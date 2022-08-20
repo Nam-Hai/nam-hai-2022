b7afa9c81df3b67003c20110614ccc5c8989c5c5
BlueTompon <lucastran00@gmail.com>
2022-08-20 18:47:07 +0200

    Component

---
+++ src/classes/Component.js
@@ -0,0 +1,17 @@
+import { N } from "../utils/namhai"
+
+export default class Component {
+  constructor({ element, content }) {
+    this.element = element
+    // this.elements = elements
+    this.content = content
+  }
+
+  create(selector) {
+    this.element = selector instanceof window.HTMLElement ? selector : N.Select(selector);
+  }
+
+  render() {
+    this.element.innerHTML = this.content
+  }
+}
