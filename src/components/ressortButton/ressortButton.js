e4d3634024ff883db9dc889d6812053b5ac2fed5
BlueTompon <lucastran00@gmail.com>
2022-08-20 21:56:26 +0200

    ressortbutton

--- src/components/ressortButton/ressortButton.js
+++ src/components/ressortButton/ressortButton.js
@@ -6,12 +6,18 @@ export default class ressortButton extends Component {
   constructor({ element }) {
 
     super({ element, content })
+
   }
 
   addEventListener() {
-    let button = N.get('button', this.element)
-    button.addEventListener('click', () => {
+    let button = N.get('.ressortButton', this.element)
+    console.log('addEventListener', this.element, button);
+    this.element.addEventListener('click', () => {
       console.log('Click le bouton');
+      button.style.backgroundColor = white
+    })
+    button.addEventListener('click', () => {
+      console.log('ressort');
     })
   }
 
