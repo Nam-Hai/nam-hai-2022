52559be5ea9f18af8b80bd88649be62a57160a6d
BlueTompon <lucastran00@gmail.com>
2022-08-20 21:56:44 +0200

    app

--- src/app.js
+++ src/app.js
@@ -2,30 +2,43 @@ import './styles/index.scss'
 import Router from './classes/Router'
 import { N } from './utils/namhai'
 import ressortButton from './components/ressortButton/ressortButton'
+import Preloader from './Pages/preloader'
 
 class App {
   constructor() {
+    this.main = N.get('main')
     this.router = new Router()
-    this.handleRoute()
+
+    this.createPreloader()
 
   }
 
-  handleRoute() {
-    console.log(this.router.path == '/');
-    let template = Object.entries(this.router.paths).filter(([path, template]) => path == this.router.path).map(a => a[1])
-    template = template.length ? template[0] : this.router.paths['/'] && window.history.pushState('historyInfo', 'Nam Hai', '/')
-
-    const divContent = N.Cr('div')
-    divContent.innerHTML = template
-    const page = N.get('#h')
-    page.innerHTML = divContent.innerHTML
-
-
-    let ressort = N.getAll('ressortButton', page)
-    console.log(ressort);
-    ressort = [...ressort].map(el => new ressortButton({ element: el }));
-    console.log(ressort);
+  createPreloader() {
+    this.preloader = new Preloader()
+    this.preloader.render(this.main)
+    console.log(this.main, this.preloader, this.preloader.content);
   }
+
+  createPage() {
+    this.template = Object.entries(this.router.paths).filter(([path, template]) => path == this.router.path).map(a => a[1])
+
+  }
+
+  // handleRoute() {
+  //   console.log(this.router.path == '/');
+  //   let template = Object.entries(this.router.paths).filter(([path, template]) => path == this.router.path).map(a => a[1])
+  //   template = template.length ? template[0] : this.router.paths['/'] && window.history.pushState('historyInfo', 'Nam Hai', '/')
+
+  //   const divContent = N.Cr('div')
+  //   divContent.innerHTML = template
+  //   const page = N.get('#h')
+  //   page.innerHTML = divContent.innerHTML
+
+
+
+  // }
+
+
 }
 
 new App()
--- src/classes/Router.js
+++ src/classes/Router.js
@@ -1,17 +1,21 @@
+import Preloader from '../Pages/preloader';
 import homeTemplate from '../views/home.html?raw'
-import preloaderTemplate from '../views/preloader.html?raw'
 
 export default class Router {
 
   constructor() {
     this.path = window.location.pathname
     console.log(this.path);
-    this.paths = {
-      '/': preloaderTemplate,
+    this.pathsManager = {
+      // '/': Preloader,
       '/home': homeTemplate
     }
 
+
+
   }
+
+
 
 }
 
