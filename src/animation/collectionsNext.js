62676c386fc1b4140e85724b560a34ea963970c7
BlueTompon <lucastran00@gmail.com>
2022-08-26 23:23:29 +0200

    transition color

--- src/animation/collectionsNext.js
+++ src/animation/collectionsNext.js
@@ -2,14 +2,25 @@ import { canvas } from "../Canvas/canvas"
 import { N } from "../utils/namhai"
 import { stringLetterToDoubleSpan } from "../utils/utilsText"
 
+const collectionsInfo = [
+  { bg: '#171717', c: '#E5DFDF', flavour: 'IA', name: 'Midjourney' },
+  { bg: '#F0BDBD', c: '#FF4A0B', flavour: 'Typography', name: 'Travis' },
+  { bg: '#131721', c: '#DA2607', flavour: 'ia', name: 'midjourney' },
+
+]
+
 let currentPage = 0
 export default class collectionsNext {
   constructor() {
+    currentPage++
     this.canvas = canvas
     console.log(this.canvas.getCurrent());
 
+    this.r = N.getAll('ressort-button')
+
+
     let bB = N.get('.buffer__background')
-    bB.style.backgroundColor = N.Ga(bB, 'data-color')
+    bB.style.backgroundColor = collectionsInfo[currentPage].bg
     this.titleWrapperBuffer = N.get('.buffer__title')
 
     let navf = N.get('.true__title .nav__title__flavour')
@@ -19,6 +30,8 @@ export default class collectionsNext {
 
     let navnB = N.get('.buffer__title .nav__title__name')
     let navfB = N.get('.buffer__title .nav__title__flavour')
+    navnB.style.color = collectionsInfo[currentPage].c
+    navfB.style.color = collectionsInfo[currentPage].c
 
     stringLetterToDoubleSpan(navfB, 'tooltip__span')
     stringLetterToDoubleSpan(navnB, 'tooltip__span')
@@ -33,6 +46,9 @@ export default class collectionsNext {
       p: {
         o: [0, 1]
       },
+      // update: t => {
+
+      // },
       e: 'io5',
     })
 
@@ -52,7 +68,7 @@ export default class collectionsNext {
         x: [-100, 0],
       },
       cb: () => {
-        this.calculNextState(navf, navn, navfB, navnB, bB)
+        this.calculNextState(navf, navn, navfB, navnB)
       },
       e: 'o5',
       delay: 200
@@ -61,20 +77,27 @@ export default class collectionsNext {
 
   }
 
-  calculNextState(navf, navn, navfB, navnB, bB) {
+  calculNextState(navf, navn, navfB, navnB) {
 
-    let a = N.get('main').style.backgroundColor = N.Ga(bB, 'data-color')
-    bB.setAttribute('data-color', '#131721')
+
+    N.get('main').style.backgroundColor = collectionsInfo[currentPage].bg
     navf.innerHTML = navfB.innerHTML
     navn.innerHTML = navnB.innerHTML
 
+    navf.style.color = collectionsInfo[currentPage].c
+    navn.style.color = collectionsInfo[currentPage].c
+
     navfB.innerHTML = 'yo'
     navnB.innerHTML = 'testyo'
-    currentPage++
+
     N.O(this.titleWrapperBuffer, 0)
   }
 
   play() {
+    this.r.forEach(r => {
+      r.style.color = collectionsInfo[currentPage].c
+      N.get('button', r).style.backgroundColor = collectionsInfo[currentPage].c
+    })
     N.O(this.titleWrapperBuffer, 1)
     this.tl.play()
   }
