214dee05418518cebd26ae90b7efd702d89f1c59
BlueTompon <lucastran00@gmail.com>
2022-08-27 23:37:45 +0200

    clg

--- src/utils/utilsText.js
+++ src/utils/utilsText.js
@@ -3,12 +3,10 @@ import { calculate } from "./text";
 
 export function stringLetterToDoubleSpan(element, className) {
 
-  console.log(element);
   let innerHTML = element.innerHTML.toString().trim()
   element.innerHTML = ''
 
   let letters = innerHTML.split('')
-  console.log(letters);
   for (const letter of letters) {
     let parent = N.Cr('span')
     parent.classList.add('doublespan__container')
