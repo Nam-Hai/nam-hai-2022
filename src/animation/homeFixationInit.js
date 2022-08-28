671ab8547276d6fea1749aef41798a0585d33a66
BlueTompon <lucastran00@gmail.com>
2022-08-28 15:06:41 +0200

    home initFixations

--- src/animation/homeFixationInit.js
+++ src/animation/homeFixationInit.js
@@ -1,23 +1,30 @@
 import { N } from "../utils/namhai";
 
 const indexToDelay = [0, 1, 2, 3, 3, 2, 1, 0]
-// export var fixationInit = false
+export var fixationInit = false
 export default class homeFixationInit {
   constructor(fixations) {
 
     this.tl = new N.TL
-    Object.entries(fixations).forEach(([index, fixation]) => {
-      this.tl.from({
-        d: 800,
-        p: {
-          x: [0, -(8 + 7 * 21) + 42 * index + 2 * (index - 1), 'px']
-        },
-        // cb: t => fixationInit = true,
-        el: fixation,
-        delay: indexToDelay[index] * 0,
-        e: 'io5',
+    if (!fixationInit) {
+
+      Object.entries(fixations).forEach(([index, fixation]) => {
+        this.tl.from({
+          d: 800,
+          p: {
+            x: [0, -(8 + 7 * 21) + 42 * index + 2 * (index - 1), 'px']
+          },
+          cb: _ => fixationInit = true,
+          el: fixation,
+          delay: indexToDelay[index] * 0,
+          e: 'io5',
+        })
       })
-    })
+    } else {
+      Object.entries(fixations).forEach(([index, fixation]) => {
+        N.T(fixation, -(8 + 7 * 21) + 42 * index + 2 * (index - 1), 0, 'px')
+      })
+    }
   }
 
   play() {
