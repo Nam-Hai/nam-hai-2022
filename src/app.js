741e4aeacfed573999450588f7ed4fe36904846a
BlueTompon <lucastran00@gmail.com>
2022-09-09 12:28:48 +0200

    app init

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ class App {
   }
 
   initPage() {
-    const url = 'contact'
+    const url = 'home'
     window.history.pushState('', 'Nam Hai portfolio', url)
     this.page = this.createPage(url)
 
